550fca528dca6b6704af0e5467ab7c5b9411c948
n8ify <nata8ify@gmail.com>
2023-04-14 22:07:11 +0700

    init

---
+++ src/main/java/com/n8ify/plantman/PlantmanApplication.java
@@ -0,0 +1,13 @@
+package com.n8ify.plantman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantmanApplication.class, args);
+	}
+
+}
